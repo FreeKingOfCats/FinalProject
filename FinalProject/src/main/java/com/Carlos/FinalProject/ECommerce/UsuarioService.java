7ad8adcfae489c91121086288b351696d0f2d249
Carlos Urrutia <carlosurrutia91@gmail.com>
2024-11-17 17:40:54 +0100

    Add files via upload

---
+++ FinalProject/src/main/java/com/Carlos/FinalProject/ECommerce/UsuarioService.java
@@ -0,0 +1,51 @@
+package com.Carlos.FinalProject.ECommerce;
+
+import org.springframework.stereotype.Service;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UsuarioService {
+    private List<Usuario> usuarios = new ArrayList<>();
+
+    public UsuarioService() {
+        loadUsuarios();
+    }
+
+    private void loadUsuarios() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(getClass().getClassLoader().getResource("Usuarios.txt").toURI()));
+            for (String line : lines) {
+                String[] details = line.replace("Id: ", "").replace("Nombre: ", "").replace("Antiguedad: ", "").split(", ");
+                Long id = Long.parseLong(details[0]);
+                String nombre = details[1];
+                int antiguedad = Integer.parseInt(details[2]);
+
+                Usuario usuario = new Usuario(id, nombre, antiguedad);
+                usuarios.add(usuario);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Usuario verificarUsuario(String nombre) {
+        Usuario usuario = usuarios.stream()
+                .filter(u -> u.getNombre().equalsIgnoreCase(nombre))
+                .findFirst()
+                .map(u -> {
+                    u.setRol("Premium");
+                    return u;
+                })
+                .orElseGet(() -> {
+                    Usuario guestUser = new Usuario(0L, nombre, 0);
+                    guestUser.setRol("Guest"); // Usuario desconocido es Guest
+                    return guestUser;
+                });
+
+        System.out.println("Usuario encontrado: " + usuario.getNombre() + ", Rol: " + usuario.getRol() + ", Antigüedad: " + usuario.getAntiguedad());
+        return usuario;
+    }
+}
